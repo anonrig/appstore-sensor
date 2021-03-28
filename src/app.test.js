614470c8e5079e438294f71c8a84fb8923339f62
Yagiz Nizipli <yagiz@nizipli.com>
2021-03-28 17:04:51 +0300

    fix: improve app tests

--- src/app.test.js
+++ src/app.test.js
@@ -6,20 +6,20 @@ describe('app', () => {
       const application = await app({ id: 284882215, include_ratings: false })
       expect(application.id).toEqual(284882215)
       expect(application.title).toEqual('Facebook')
+      done()
     } catch (error) {
       expect(error).toBeUndefined()
-    } finally {
-      done()
+      done(error)
     }
   })
 
   test('should throw error on not found', async (done) => {
     try {
       await app({ id: '123456789' })
+      done(new Error(`Invalid`))
     } catch (error) {
       expect(error).toBeDefined()
       expect(error.message).toEqual('Application not found')
-    } finally {
       done()
     }
   })
@@ -33,19 +33,19 @@ describe('app', () => {
       expect(application.reviews).toBeTruthy()
       expect(application.histogram).toBeDefined()
       expect(application.ratings).toBeDefined()
+      done()
     } catch (error) {
       expect(error).toBeUndefined()
-    } finally {
-      done()
+      done(error)
     }
   })
 
   test('should set request options', async (done) => {
     try {
       await app({ id: 284882215 }, { method: 'DELETE' })
+      done(new Error(`Invalid`))
     } catch (error) {
       expect(error).toBeDefined()
-    } finally {
       done()
     }
   })
