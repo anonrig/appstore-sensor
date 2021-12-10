4e473540d223053a49dab99ec27b5ce806d1744f
Yagiz Nizipli <yagiz@nizipli.com>
2021-12-10 16:52:43 -0500

    fix: remove invalid reference to ratings.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import app from './src/app.js'
-import ratings from './src/ratings.js'
 import reviews from './src/reviews.js'
 import developer from './src/developer.js'
 import search from './src/search.js'
 
 export default {
   app,
-  ratings,
   reviews,
   developer,
   search,
